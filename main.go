5456cce25b492ad92ae14a8f2b67017c69fbd69a
abakum <koka.abakum@gmail.com>
2023-06-20 10:28:17 +0300

    7 7000

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/xlab/closer"
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	ansiReset = "\u001B[0m"
+	ansiRedBG = "\u001B[41m"
+	BUG       = ansiRedBG + "Ж" + ansiReset
+)
+
+var (
+	// letf    = log.New(os.Stdout, BUG, log.Ltime|log.Lshortfile)
+	ltf     = log.New(os.Stdout, " ", log.Ltime|log.Lshortfile)
+	let     = log.New(os.Stdout, BUG, log.Ltime)
+	lt      = log.New(os.Stdout, " ", log.Ltime)
+	hub4com = `..\hub4com\hub4com.exe`
+	com     = "7"
+	port    = "7000"
+	//go:embed NGROK_API_KEY.txt
+	NGROK_API_KEY string
+	//go:embed NGROK_AUTHTOKEN.txt
+	NGROK_AUTHTOKEN string
+)
+
+func main() {
+	var (
+		err error
+	)
+	defer closer.Close()
+
+	closer.Bind(func() {
+		if err != nil {
+			let.Println(err)
+			defer os.Exit(1)
+		}
+	})
+
+	cwd, err := os.Getwd()
+	if err == nil {
+		hub4com = filepath.Join(cwd, hub4com)
+	}
+
+	_, forwardsTo, err := ngrokAPI()
+	if err == nil {
+		err = Errorf("found online client: %s", forwardsTo)
+		return
+	}
+
+	hub := exec.Command(
+		hub4com,
+		"--create-filter=escparse,com,parse",
+		"--create-filter=purge,com,purge",
+		"--create-filter=pinmap,com,pinmap:--rts=cts --dtr=dsr --break=break",
+		"--create-filter=linectl,com,lc:--br=remote --lc=remote",
+		"--add-filters=0:com",
+		"--create-filter=telnet,tcp,telnet:--comport=server --suppress-echo=yes",
+		"--create-filter=lsrmap,tcp,lsrmap",
+		"--create-filter=pinmap,tcp,pinmap:--cts=cts --dsr=dsr --dcd=dcd --ring=ring",
+		"--create-filter=linectl,tcp,lc:--br=local --lc=local",
+		"--add-filters=1:tcp",
+		"--octs=off",
+		"COM"+com,
+		"--use-driver=tcp",
+		"*"+port,
+	)
+	hub.Stdout = os.Stdout
+	hub.Stderr = os.Stderr
+	err = hub.Start()
+	if err != nil {
+		err = srcError(err)
+		return
+	}
+	closer.Bind(func() {
+		PrintOk("hub4com", hub.Process.Kill())
+	})
+
+	PrintOk("ngrok", run(context.Background(), "127.0.0.1:"+port))
+}
+
+// https://github.com/ngrok/ngrok-go/blob/main/examples/ngrok-lite/main.go
+func run(ctx context.Context, dest string) error {
+	tun, err := ngrok.Listen(ctx,
+		config.TCPEndpoint(),
+		ngrok.WithAuthtoken(Getenv("NGROK_AUTHTOKEN", NGROK_AUTHTOKEN)),
+	)
+	if err != nil {
+		return err
+	}
+
+	ltf.Println("tunnel created:", tun.URL())
+
+	for {
+		conn, err := tun.Accept()
+		if err != nil {
+			return err
+		}
+
+		ltf.Println("accepted connection from", conn.RemoteAddr())
+
+		go func() {
+			err := handleConn(ctx, dest, conn)
+			ltf.Println("connection closed:", err)
+		}()
+	}
+}
+
+func handleConn(ctx context.Context, dest string, conn net.Conn) error {
+	next, err := net.Dial("tcp", dest)
+	if err != nil {
+		return err
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		_, err := io.Copy(next, conn)
+		return err
+	})
+	g.Go(func() error {
+		_, err := io.Copy(conn, next)
+		return err
+	})
+
+	return g.Wait()
+}
---
+++ util.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/ngrok/ngrok-api-go/v5"
+	"github.com/ngrok/ngrok-api-go/v5/tunnels"
+)
+
+// Get source of code
+func src(deep int) (s string) {
+	s = string(debug.Stack())
+	str := strings.Split(s, "\n")
+	if l := len(str); l <= deep {
+		deep = l - 1
+		for k, v := range str {
+			fmt.Println(k, v)
+		}
+	}
+	s = str[deep]
+	s = strings.Split(s, " +0x")[0]
+	_, s = path.Split(s)
+	s += ":"
+	return
+}
+
+// Wrap source of code and message to error
+func Errorf(format string, args ...any) error {
+	return fmt.Errorf(src(8)+" %w", fmt.Errorf(format, args...))
+}
+
+// Wrap source of code and error to error
+func srcError(err error) error {
+	return fmt.Errorf(src(8)+" %w", err)
+}
+
+func Getenv(key, val string) string {
+	s := os.Getenv(key)
+	if s == "" {
+		return val
+	}
+	return s
+}
+
+func ngrokAPI() (publicURL string, forwardsTo string, err error) {
+	NGROK_API_KEY = Getenv("NGROK_API_KEY", NGROK_API_KEY)
+	if NGROK_API_KEY == "" {
+		return "", "", Errorf("not NGROK_API_KEY in env")
+	}
+
+	// construct the api client
+	clientConfig := ngrok.NewClientConfig(NGROK_API_KEY)
+
+	// list all online client
+	client := tunnels.NewClient(clientConfig)
+	iter := client.List(nil)
+	err = iter.Err()
+	if err != nil {
+		return "", "", srcError(err)
+	}
+
+	ctx, ca := context.WithTimeout(context.Background(), time.Second*3)
+	defer ca()
+	for iter.Next(ctx) {
+		err = iter.Err()
+		if err != nil {
+			return "", "", srcError(err)
+		}
+		if true { //free version allow only one tunnel
+			return iter.Item().PublicURL, iter.Item().ForwardsTo, nil
+		}
+	}
+	return "", "", Errorf("not found online client")
+}
+
+func PrintOk(s string, err error) {
+	if err != nil {
+		let.Println(src(8), s, err)
+	} else {
+		lt.Println(src(8), s, "ok")
+	}
+}
